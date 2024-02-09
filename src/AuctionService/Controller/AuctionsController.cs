2a1524261b7fba9f5e896596fd7219816b969650
Tung <phamthanhtungqptb@gmail.com>
2024-02-09 16:06:35 +0700

    End p3

--- src/AuctionService/Controller/AuctionsController.cs
+++ src/AuctionService/Controller/AuctionsController.cs
@@ -7,6 +7,7 @@ using AuctionService.Data;
 using AuctionService.DTOs;
 using AuctionService.Entities;
 using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -27,13 +28,16 @@ namespace AuctionService.Controller
         }
         
         [HttpGet]
-        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(){
-            var auctions = await _context.Auctions
-                .Include(x => x.Item)
-                .OrderBy(x => x.Item.Make)
-                .ToListAsync();
+        public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions(string date)
+        {
+        var query = _context.Auctions.OrderBy(x => x.Item.Make).AsQueryable();
 
-            return _mapper.Map<List<AuctionDto>>(auctions);
+        if (!string.IsNullOrEmpty(date))
+        {
+            query = query.Where(x => x.UpdatedAt.CompareTo(DateTime.Parse(date).ToUniversalTime()) > 0);
+        }
+
+        return await query.ProjectTo<AuctionDto>(_mapper.ConfigurationProvider).ToListAsync();
         }
 
         [HttpGet("{id}")]
